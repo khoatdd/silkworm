81f7923950f69868b295c77d01fe8bc4d1b1482f
legenddotavn <dangkhoa114@gmail.com>
2017-09-10 10:07:02 +0700

    Add __init__.py and __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Screen Printing',
+    'version': '1.0',
+    'description': """This module adds functionality for screen printing
+     companies""",
+    'author': 'Khoa Than Doan Dang',
+    'depends': ['base', 'sale'],
+    'data': ['silkworm_view.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
